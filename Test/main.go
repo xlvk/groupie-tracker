7751966563b44d9b38fa228ec0b826fd31cd138c
fatabbas <fatima77614@gmail.com>
2023-10-24 17:23:19 +0300

    3

--- Test/main.go
+++ Test/main.go
@@ -6,7 +6,16 @@ import (
 	"html/template"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"log"
+	"os"
 )
+
+var sere *template.Template
+
+func init() {
+	sere = template.Must(template.ParseGlob("index.html"))
+}
 
 type GroupieData struct {
 	Artists  string `json:"artists"`
@@ -16,17 +25,36 @@ type GroupieData struct {
 }
 
 func main() {
-
+	http.HandleFunc("/", groupieHandler)
+	// http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css/"))))
+	http.HandleFunc("/groupie-tracker", processor)
+	u, err := url.Parse("http://localhost:2003")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Print("Listening and serving on: ")
+	fmt.Printf("%+v", u)
+	fmt.Println()
+	log.Fatal(http.ListenAndServe(":2003", nil))
 }
 
 func groupieHandler(w http.ResponseWriter, r *http.Request) {
 	// Locate and Read JSON File
-	fileData := "GroupieData.json"
-	data, err := ioutil.ReadFile(fileData)
+
+	fileData, err := http.Get("https://groupietrackers.herokuapp.com/api")
+
+    if err != nil {
+        fmt.Print(err.Error())
+        os.Exit(1)
+    }
+
+	// fileData := ""
+	data, err := ioutil.ReadAll(fileData.Body)
 	if err != nil {
 		fmt.Println("Error: ", err)
 	}
 
+	fmt.Println(string(data))
 	// Parse JSON Data
 	var groupieData []GroupieData
 	err = json.Unmarshal(data, &groupieData)
@@ -55,4 +83,29 @@ func groupieHandler(w http.ResponseWriter, r *http.Request) {
 	// for _, group := range groupieData {
 	// 	fmt.Println(group.Artists, group.Dates, group.Location, group.Relation)
 	// }
+}
+
+
+func processor(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		if r.URL.Path != "/" && r.URL.Path != "/groupie-tracker" {
+			// w.Header().Set("Content-Type", "html/text")
+			w.WriteHeader(http.StatusNotFound)
+			http.ServeFile(w, r, "404Error.html")
+		}
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		http.ServeFile(w, r, "405Error.html")
+	}
+	ha := r.FormValue("asciiBanner")
+	
+	d := struct {
+		Result string
+	}{
+		Result: ha,
+	}
+	tmp, _ := template.ParseFiles("processor.html")
+
+	tmp.Execute(w, d)
+
 }
