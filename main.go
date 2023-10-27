867f739465f537a8c2b542674de2fa5815bb697a
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-10-27 15:56:00 +0000

    4

--- main.go
+++ main.go
@@ -1,28 +1,58 @@
 package main
 
 import (
-	// "bufio"
+	"encoding/json"
 	"fmt"
 	"html/template"
-	// "io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-	// "os"
-	// "strings"
+	"os"
 )
 
 var sere *template.Template
 
 func init() {
-	sere = template.Must(template.ParseGlob("template/index.html"))
+	sere = template.Must(template.ParseGlob("index.html"))
+}
+
+// type GroupieData struct {
+// 	Artists   string `json:"artists"`
+// 	Locations string `json:"locations"`
+// 	Dates     string `json:"dates"`
+// 	Relation  string `json:"relation"`
+// }
+
+type GroupieData struct {
+	Artists   string `json:"artists"`
+	Locations string `json:"locations"`
+	Dates     string `json:"dates"`
+	Relation  string `json:"relation"`
+}
+
+type ArtistsAPI struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
+
+type LocationsAPI struct {
+	ID        int      `json:"id"`
+	Locations []string `json:"locations"`
+	Dates     string   `json:"dates"`
 }
 
 func main() {
-	http.HandleFunc("/", Index)
-	http.Handle("/template/css/", http.StripPrefix("/template/css/", http.FileServer(http.Dir("css/"))))
+	http.HandleFunc("/", groupieHandler)
+	// http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css/"))))
 	http.HandleFunc("/groupie-tracker", processor)
-	// http.HandleFunc("/download", DownLoad)
 	u, err := url.Parse("http://localhost:2003")
 	if err != nil {
 		fmt.Println(err)
@@ -31,26 +61,58 @@ func main() {
 	fmt.Printf("%+v", u)
 	fmt.Println()
 	log.Fatal(http.ListenAndServe(":2003", nil))
-
 }
 
+func groupieHandler(w http.ResponseWriter, r *http.Request) {
+	// Locate and Read JSON File
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	FromJasonToTxt(w)
-	if r.Method == "GET" {
-		if r.URL.Path == "/" {
-			sere.ExecuteTemplate(w, "index.html", nil)
-			return
-		} else {
-			// w.Header().Set("Content-Type", "html/text")
-			w.WriteHeader(http.StatusNotFound)
-			http.ServeFile(w, r, "template/404Error.html")
-		}
-	} else {
-		// w.Header().Set("Content-Type", "html/text")
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		http.ServeFile(w, r, "template/405Error.html")
+	fileData, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
 	}
+	defer fileData.Body.Close()
+
+	// fileData := ""
+	data, err := ioutil.ReadAll(fileData.Body)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	//fmt.Println(string(data))
+	// Parse JSON Data
+	var groupieData []ArtistsAPI
+	err = json.Unmarshal(data, &groupieData)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	fmt.Print(groupieData[0].Members)
+	fmt.Print(groupieData[0].Name)
+	fmt.Print(groupieData[0].Locations)
+	fmt.Print(groupieData[0].Relations)
+
+	// Prepare Data For HTML
+	type DataView struct {
+		Groupie []ArtistsAPI
+	}
+	viewData := DataView{
+		Groupie: groupieData,
+	}
+
+	// Load HTML Page
+	pageView := template.Must(template.ParseFiles("index.html"))
+
+	// Execute HTML with Data
+	err = pageView.Execute(w, viewData)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	// Print Data on Terminal
+	// for _, group := range groupieData {
+	// 	fmt.Println(group.Artists, group.Dates, group.Location, group.Relation)
+	// }
 }
 
 func processor(w http.ResponseWriter, r *http.Request) {
@@ -58,29 +120,21 @@ func processor(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" && r.URL.Path != "/groupie-tracker" {
 			// w.Header().Set("Content-Type", "html/text")
 			w.WriteHeader(http.StatusNotFound)
-			http.ServeFile(w, r, "template/404Error.html")
+			http.ServeFile(w, r, "404Error.html")
 		}
 		w.Header().Set("Content-Type", "text/html")
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		http.ServeFile(w, r, "template/405Error.html")
+		http.ServeFile(w, r, "405Error.html")
 	}
 	ha := r.FormValue("asciiBanner")
-	
+
 	d := struct {
 		Result string
 	}{
 		Result: ha,
 	}
-	tmp, _ := template.ParseFiles("template/processor.html")
+	tmp, _ := template.ParseFiles("processor.html")
 
 	tmp.Execute(w, d)
 
 }
-
-func ErrorPage(w http.ResponseWriter) {
-	w.WriteHeader(http.StatusInternalServerError)
-	template.Must(template.ParseGlob("template/500Error.html"))
-	sere.ExecuteTemplate(w, "template/500Error.html", nil)
-}
-
-
